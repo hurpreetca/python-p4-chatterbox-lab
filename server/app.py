97d34012a218651689e24306a6166c9600decc78
Harpreet Singh <hurpreet@icloud.com>
2023-09-26 16:55:58 -0400

    Chatbox lab completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,78 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages= []
+        for message in Message.query.all():
+            dict= message.to_dict()
+            messages.append(dict)
+        response = make_response(messages, 200)
 
-@app.route('/messages/<int:id>')
+    elif request.method == "POST":
+        request_dict = request.get_json()
+        new_message = Message(
+            body = request_dict.get("body"), username=request_dict.get("username")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        dict = new_message.to_dict()
+
+        response = make_response(dict, 201)
+    
+    return response
+    
+        
+
+@app.route('/messages/<int:id>', methods=["GET","PATCH","DELETE"])
 def messages_by_id(id):
-    return ''
+    message= Message.query.filter(Message.id == id).first()
+
+    if message == None:
+        response_body={
+            "message": "This message does exist in our chatbox. Please try again!"
+            }
+        response= make_response(jsonify(response_body), 404)
+
+    
+    else:
+        if request.method == "GET":
+            message_dict = message.to_dict()
+
+            response= make_response(message_dict, 200)
+
+
+        
+        elif request.method == "PATCH":
+            request_dict = request.get_json()
+
+            for attr in request_dict:
+                setattr(message, attr, request_dict.get(attr))
+            
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict= message.to_dict()
+
+            response= make_response(message_dict, 200)
+
+        
+        elif request.method == "DELETE":
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body= {
+                "delete_successful": True,
+                "message": "Review deleted"
+            }
+
+            response = make_response(response_body, 200)
+
+    return response
+        
+
 
 if __name__ == '__main__':
     app.run(port=5555)
